4b9141f2a934c13f386d1e672734d8ab92065771
Subhadra Mishra <subhadramishra1994@gmail.com>
2025-12-29 17:33:11 -0500

    Make resume section links and navbar items more clickable with hover effects, move GPA to top-right in education

--- src/components/Resume/Education/Degree.js
+++ src/components/Resume/Education/Degree.js
@@ -4,7 +4,12 @@ import PropTypes from 'prop-types';
 const Degree = ({ data }) => (
   <article className="degree-container">
     <header>
-      <h4 className="degree">{data.degree}</h4>
+      <div className="degree-header">
+        <h4 className="degree">{data.degree}</h4>
+        {data.gpa && (
+          <span className="degree-gpa">GPA: {data.gpa}</span>
+        )}
+      </div>
       <p className="school">
         <a href={data.link}>{data.school}</a>, {data.year}
       </p>
@@ -15,6 +20,7 @@ const Degree = ({ data }) => (
 Degree.propTypes = {
   data: PropTypes.shape({
     degree: PropTypes.string.isRequired,
+    gpa: PropTypes.string,
     link: PropTypes.string.isRequired,
     school: PropTypes.string.isRequired,
     year: PropTypes.number.isRequired,
--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,13 +1,15 @@
 const degrees = [
   {
     school: 'Indiana University Bloomington',
-    degree: 'M.S. Computer Science (GPA: 3.6/4.00)',
+    degree: 'M.S. Computer Science',
+    gpa: '3.6/4.00',
     link: 'https://luddy.indiana.edu',
     year: 2025,
   },
   {
     school: 'Indian Institute of Technology (IIT) Guwahati',
-    degree: 'M.S. Mathematics and Computing (GPA: 7.25/10)',
+    degree: 'M.S. Mathematics and Computing',
+    gpa: '7.25/10',
     link: 'https://www.iitg.ac.in',
     year: 2018,
   },
